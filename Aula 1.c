807025610ebafad2874e9c59994fd81ef043639f
Vinicius dos Santos Fuzinatto <fuzivini@gmail.com>
2026-08-06 11:31:38 -0300

    Add files via upload

---
+++ Aula 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	 int a,b,c; //as variáveis são separadas entre vírgulas pra reprensentar os espaços separados que os dados serão gardados na memória.
+	 
+	 a = 8;
+	 b = 19;
+	 c = a+b;
+	 
+	 printf("A soma de %d + %d = %d", a,b,c);
+	
+	return 0;
+}
