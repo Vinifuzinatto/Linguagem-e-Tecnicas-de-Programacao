6fc39dffb180e34043a625791b3b02d1a2d3e7ea
Vinicius dos Santos Fuzinatto <fuzivini@gmail.com>
2026-08-26 11:27:26 -0300

    Add files via upload

---
+++ Exercicios aula.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+/*Exercício 10 (pt.2): fazendo com condicional agr */
+	
+	/*int a, b, c, r;
+	
+	printf("Entre com os valores A B C : ");
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if (a>b){
+		r = a;
+	}
+	else{
+		r = b;
+	}
+	if (c>r){
+		r = c;
+	}
+	
+	printf("%d eh o maior!!!", r);
+	
+
+
+/*Exercício aula: Ler um número e informar se ele é par ou ímpar. */
+
+	/*int numero;
+	
+	printf("Insira um valor: ");
+	scanf("%d", &numero);
+	
+	if (numero % 2 == 0){
+		printf("O numero e par!!!");
+	}
+	else
+	printf("O numero e impar!!!");
+	
+
+
+/*Exercício aula 2: Faça um programa que leia do teclado um letra e imprima a sua correspondência no "Abecedário da Xuxa". */
+
+	char letra;
+	
+	printf("Insira uma letra do alfabeto: ");
+	scanf("%c", &letra);
+	
+	
+	
+	return 0;
+}
