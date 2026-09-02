d5a9ed32b05819d783207426496a1a7760b202d9
Vinicius dos Santos Fuzinatto <fuzivini@gmail.com>
2026-09-02 19:49:59 -0300

    Update 02. Exercício 3 (Quadrado Perfeito).c

--- Atividades Aula Estrutura Codicional/02. Exercício 3 (Quadrado Perfeito).c
+++ Atividades Aula Estrutura Codicional/02. Exercício 3 (Quadrado Perfeito).c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-//Exerc�cio 3: Leia um n�mero e, se ele for um quadrado perfeito, informe sua raiz.
+//Exercício 3: Leia um número e, se ele for um quadrado perfeito, informe sua raiz.
 
 int main() {
 	
@@ -10,9 +10,12 @@ int main() {
 	
 	printf("Insira um numero: ");
 	scanf("%f", &n);
+
+	raiz = sqrt(n);
 	
-	if (raiz == (int)raiz){
-		raiz = sqrt(n);
+	if (raiz == (int)raiz) { //Compara a raiz feita anteriormente, com a variável n, tirando sua parte decimal. Se a raiz n for igual a raiz n só com a parte inteira, logo, o número é um quadrado perfeito.
+							 //Exemplo 1: número = 4; Raiz = 2, (int)Raiz = 2 =) VERDADEIRO (Pois Raiz == (int)Raiz) [2 = 2]
+							 //Exemplo 2: número = 3; Raiz = 1.7, (int)Raiz = 1 =) Falso (Pois Raiz != (int)Raiz) [1.7 != 1]
 		printf("\nRaiz = %0.2f", raiz);
 	}
 	else {
